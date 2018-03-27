e12f897df43a4c52453c7ab7ab984352e158073d
TeodorKolev <teodor.kolev@silicon-nile.com>
2018-03-27 23:40:36 +0300

    adding user

--- server.js
+++ server.js
@@ -1,17 +1,29 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var cors = require('cors');
+let http = require('http'),
+    path = require('path'),
+    methods = require('methods'),
+    express = require('express'),
+    bodyParser = require('body-parser'),
+    session = require('express-session'),
+    cors = require('cors'),
+    passport = require('passport'),
+    errorhandler = require('errorhandler'),
+    mongoose = require('mongoose');
+
+let isProduction = process.env.NODE_ENV === 'production';
 
 // create express app
-var app = express();
+let app = express();
+
 app.use(cors());
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.json({limit: '2mb'}));
+// Normal express config defaults
+app.use(require('morgan')('dev'));
 app.use(bodyParser.urlencoded({limit: '2mb', extended: true}));
+app.use(bodyParser.json({limit: '2mb'}));
 
-// parse application/json
-app.use(bodyParser.json())
+
+// TODO continue here
 
 // Configuring the database
 var dbConfig = require('./config/database.config.js');
@@ -38,7 +50,71 @@ app.get('/', function(req, res){
 
 require('./app/routes/node.routes.js')(app);
 
-// listen for requests
-app.listen(3000, function(){
-    console.log("Server is listening on port 3000");
+
+/*/===========================================================*/
+
+
+
+
+
+app.use(require('method-override')());
+app.use(express.static(__dirname + '/public'));
+
+app.use(session({ secret: 'conduit', cookie: { maxAge: 60000 }, resave: false, saveUninitialized: false  }));
+
+if (!isProduction) {
+    app.use(errorhandler());
+}
+
+if(isProduction){
+    mongoose.connect(process.env.MONGODB_URI);
+} else {
+    mongoose.connect('mongodb://localhost/conduit');
+    mongoose.set('debug', true);
+}
+
+require('./models/User');
+require('./models/Article');
+require('./models/Comment');
+require('./config/passport');
+
+app.use(require('./routes'));
+
+/// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+/// error handlers
+
+// development error handler
+// will print stacktrace
+if (!isProduction) {
+    app.use(function (err, req, res, next) {
+        console.log(err.stack);
+        res.status(err.status || 500);
+        res.json({
+            'errors': {
+                message: err.message,
+                error: err
+            }
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.json({'errors': {
+            message: err.message,
+            error: {}
+        }});
+});
+
+// finally, let's start our server...
+var server = app.listen( process.env.PORT || 3000, function(){
+    console.log('Listening on port ' + server.address().port);
 });
