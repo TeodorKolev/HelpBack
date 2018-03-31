95f526ab6f0ddd45767ba0b8d1fc8a97fee74c1d
TeodorKolev <teodor.kolev@silicon-nile.com>
2018-03-31 19:53:57 +0300

    added status enums

--- models/Node.js
+++ models/Node.js
@@ -5,7 +5,7 @@ var NodeSchema = new mongoose.Schema({
   name: {type: String, required: [true, "can't be blank"], match: [/^[a-zA-Zа-яА-Я '-]{5,200}$/, 'is invalid']},
   image: String,
   description: {type: String, required: false, match: [/^[a-zA-Zа-яА-Я0-9 '-.!"]{0,1000}$/, 'is invalid']},
-  status: { type: 'String', required: false },
+  status: {type: String, enum: ['ACTIVE', 'INACTIVE']},
   bank: {type: String, match: [/^[a-zA-Zа-яА-Я '-]{0,200}$/, 'is invalid']},
   iban: {type: String, required: [true, "can't be blank"], match: [/^[a-zA-Z0-9 -]{5,50}$/, 'is invalid']},
   bic: {type: String, match: [/^[a-zA-Z0-9 -]{0,50}$/, 'is invalid']},
